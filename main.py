1a55ea79381dda8e8a500f1aa7d9469a5f72afae
LukasBuehlmann <buehlmal@mcmaster.ca>
2025-01-11 17:04:55 -0500

    Initial venv setup

---
+++ main.py
@@ -0,0 +1,14 @@
+from queue import Queue
+
+import os
+import numpy as np
+from openai import OpenAI
+import speech_recognition as sr
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
